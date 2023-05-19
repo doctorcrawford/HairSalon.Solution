bf3e40f363d426738a27be30694ff2e8a44788e8
Kyle Crawford <kcraw217@gmail.com>
2023-05-19 15:32:55 -0700

    add more links, add Specialty field for stylists, add Haircut field for Clients

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -6,6 +6,7 @@ namespace HairSalon.Models
   {
     public int ClientId { get; set; }
     public string Name { get; set; }
+    public string Haircut { get; set; }
     public int StylistId { get; set; }
     public Stylist Stylist { get; set; }
   }
